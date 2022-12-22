e112d1f2660a793ef8619ccfdbfceb643d6c3d30
ls <coderlius@qq.com>
2022-12-22 12:17:47 +0800

    feat: 更新rer

--- src/service.js
+++ src/service.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+axios.defaults.timeout = 3e3;
+axios.defaults.withCredentials = true;
+
 export function aRequest(path, options) {
     return axios({
-        url: `${window.__er__.BASE_URL}${path}`,
-        ...options,
+         url: `${window.__er__.BASE_URL || ''}${path}`,
+         ...options,
+         withCredentials: true,
     }).then(res => res.data);
 }
